c5400c37349c0304371f386146712553ced1c1b4
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-21 09:40:05 -0700

    ByteCode -> Python #1

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     return 98 + (a ** b)
-print(magic_calculation(2, 3))
+def run_tests():
+    result1 = magic_calculation(2, 3)
+    print(result1)
+    result2 = magic_calculation(1, 100)
+    print(result2)
+    result3 = magic_calculation(10, 10)
+    print(result3)
+    result4 = magic_calculation(1, 1)
+    print(result4)  
+if __name__ == "__main__":
+    run_tests()
