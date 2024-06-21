272a51ec886ca05b0b5c8107673942898e7995b6
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-21 04:18:41 -0700

    commits

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"word_first_3: {word[0:3]}")
-print(f"word_last_2: {word[7:9]}")
-print(f"middle_word: {word[1:8]}")
+print(f"First 3 letters: {word[0:3]}")
+print(f"Last 2 letters: {word[7:9]}")
+print(f"Middle word: {word[1:8]}")
