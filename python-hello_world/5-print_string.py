62569261db56ffdfe50c79c8153d1f99ae5187fb
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-21 02:50:24 -0700

    commits

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton school"
-print(f" {str[0:9] + str[9:] + str[:9] + str[9:] + str[:9] + str[9:]}\n")
+print(f" {str[0:9] + str[9:] + str[:9] + str[9:] + str[:9] + str[9:]}")
 print(f" {str[0:9]}")
