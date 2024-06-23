438b1fc0fb8c0140d9b867304d447dd4e979e72e
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-23 11:04:29 -0700

    updated 8. islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def islower(c):
-    if len(c) != 1:
-        return "{} => upper".format(repr(c))
-    elif 'a' <= c <= 'z':
-        return "{} => lower".format(repr(c))
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
     else:
-        return "{} => upper".format(repr(c))
-
-# Test cases
-print("a is {}".format(islower("a")))
-print("H is {}".format(islower("H")))
-print("A is {}".format(islower("A")))
-print("3 is {}".format(islower("3")))
-print("g is {}".format(islower("g")))
+        return False
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
