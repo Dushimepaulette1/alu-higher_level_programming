141757eab79e93107bc359dcc277b426f7a6a94f
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-28 02:51:40 -0700

    Infinite addition

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     # Retrieve the arguments
     args = sys.argv[1:]  # Exclude the script name itself
     # Convert arguments to integers and calculate the sum
-    total_sum = sum(int(arg) for arg in args)  
+    total_sum = sum(int(arg) for arg in args)
     # Print the result
     print(total_sum)
