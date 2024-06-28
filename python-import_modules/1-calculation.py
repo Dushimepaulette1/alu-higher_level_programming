25ca97aed2a731cb3c698f1b6c189b538f8ab12d
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-28 01:35:17 -0700

    1. My first toolbox!

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,13 +5,13 @@ a = 10
 b = 5
 
 # Import the functions from calculator_1
-from calculator_1 import add, sub, mul, div
+import calculator_1
 
 # Perform the calculations
-result_add = add(a, b)
-result_sub = sub(a, b)
-result_mul = mul(a, b)
-result_div = div(a, b)
+result_add = calculator_1.add(a, b)
+result_sub = calculator_1.sub(a, b)
+result_mul = calculator_1.mul(a, b)
+result_div = calculator_1.div(a, b)
 
 # Print the results
 print("{} + {} = {}".format(a, b, result_add))
