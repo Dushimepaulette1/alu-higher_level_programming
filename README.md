3bce306cd5244505045edc3692fe287a64e6342d
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-28 01:50:11 -0700

    1. My first toolbox!

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a repo about shell scripting and python scripts 
--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
 
-# Define the variables
-a = 10
-b = 5
-
-# Import the functions from calculator_1
-from calculator_1 import add, sub, mul, div
-
-# Perform the calculations
-result_add = add(a, b)
-result_sub = sub(a, b)
-result_mul = mul(a, b)
-result_div = div(a, b)
-
-# Print the results
-print("{} + {} = {}".format(a, b, result_add))
-print("{} - {} = {}".format(a, b, result_sub))
-print("{} * {} = {}".format(a, b, result_mul))
-print("{} / {} = {}".format(a, b, result_div))
-
 if __name__ == "__main__":
-    pass
+    """
+    Imports functions from a module, does some Maths.
+    """
+    from calculator_1 import add, sub, mul, div
+    
+    # Define the variables
+    a = 10
+    b = 5
+    
+    # Perform the calculations and print the results
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
