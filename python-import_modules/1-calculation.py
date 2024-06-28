dbb4c8190195c7747617314920f4433009880aed
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-28 01:17:23 -0700

    1. My first toolbox!

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+# Define the variables
+a = 10
+b = 5
+
+# Import the functions from calculator_1
+from calculator_1 import add, sub, mul, div
+
+# Perform the calculations
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+# Print the results
+print(f"{a} + {b} = {result_add}")
+print(f"{a} - {b} = {result_sub}")
+print(f"{a} * {b} = {result_mul}")
+print(f"{a} / {b} = {result_div}")
+
+if __name__ == "__main__":
+    pass
