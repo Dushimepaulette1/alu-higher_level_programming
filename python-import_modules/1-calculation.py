0f03bd43678e7a5f879b50741d0a7765d0298f64
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-28 02:16:18 -0700

    updates 1. My first toolbox!

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 
-# Define the variables
-a = 10
-b = 5
-
-# Import functions from the module calculator_1
-from calculator_1 import add, sub, mul, div
-
-# Perform the calculations
-result_add = add(a, b)
-result_sub = sub(a, b)
-result_mul = mul(a, b)
-result_div = div(a, b)
-
-# Print the results
-print("{} + {} = {}".format(a, b, result_add))
-print("{} - {} = {}".format(a, b, result_sub))
-print("{} * {} = {}".format(a, b, result_mul))
-print("{} / {} = {}".format(a, b, result_div))
-
 if __name__ == "__main__":
-    pass
+    from calculator_1 import add, sub, mul, div
+
+    # Define the variables
+    a = 10
+    b = 5
+
+    # Perform the calculations
+    result_add = add(a, b)
+    result_sub = sub(a, b)
+    result_mul = mul(a, b)
+    result_div = div(a, b)
+
+    # Print the results using string formatting
+    print("{} + {} = {}".format(a, b, result_add))
+    print("{} - {} = {}".format(a, b, result_sub))
+    print("{} * {} = {}".format(a, b, result_mul))
+    print("{} / {} = {}".format(a, b, result_div))
