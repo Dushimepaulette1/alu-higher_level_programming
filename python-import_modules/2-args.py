bc1300283942d201a966f457917ba2d4053b28ad
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-06-28 02:29:38 -0700

    updates

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,10 @@ import sys
 if __name__ == "__main__":
     # Retrieve the arguments
     argv = sys.argv[1:]  # Exclude the script name itself
-    
+
     # Count the number of arguments
     num_args = len(argv)
-    
+
     # Print the number of arguments
     if num_args == 0:
         print("0 arguments.")
@@ -17,7 +17,8 @@ if __name__ == "__main__":
             print("1 argument:")
         else:
             print(f"{num_args} arguments:")
-        
+
         # Print each argument with its position
         for i, arg in enumerate(argv, start=1):
             print(f"{i}: {arg}")
+
