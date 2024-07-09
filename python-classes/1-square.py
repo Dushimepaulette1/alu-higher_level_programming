add38bbbc8e15404bb9e5321e119e2ac7d567b7a
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-07-09 06:13:40 -0700

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Represent the class square"""
+    def __init__(self, size):
+        """Initialize the Square.
+        Arguments:
+            size (int): The size of the square.
+        """
+        self.__size = size
