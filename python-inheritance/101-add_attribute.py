a70b1081d5c585dbbb20d6b6eb1139c05b2e82fa
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-07-11 09:29:59 -0700

    mandatory tasks

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Function that adds a new attribute if possible """
+
+def add_attribute(obj, attr, value):
+    """ Adds new attribute to the object if possible """
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    elif hasattr(obj, attr):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attr, value)
