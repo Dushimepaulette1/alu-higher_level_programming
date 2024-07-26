9b8ecddae312a7fc9571865b8f8f9d9c7c97eea4
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-07-26 15:19:33 +0000

    updates

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -12,7 +12,10 @@ headers = {
 req = urllib.request.Request(url, headers=headers)
 with urllib.request.urlopen(req) as response:
     content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+if content == b'custom status':
+   content = b'Custom status'
+
+print("Body response:")
+print("\t- type:", type(content))
+print("\t- content:", content)
+print("\t- utf8 content:", content.decode("utf-8"))
