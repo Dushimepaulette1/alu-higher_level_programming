7f0cf2042b9afe5b8e5382f086aa79461d31188c
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-07-30 20:18:56 +0000

    python network 1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Documented"""
+"""I document you"""
+
+
 import urllib.request
 
-url = 'https://alu-intranet.hbtn.io/status'
 
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    content = response.read()
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
-
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
