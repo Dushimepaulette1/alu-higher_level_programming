bd0da7c7c4ffe8ede91b38f7808f62ce7719c13c
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-07-31 01:54:17 -0700

    advanced tasks

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
-Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-import sys
+Module 100-github_commits.py
+"""
+
+
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
-
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+            argv[2], argv[1])
+    resp = requests.get(url)
+    data = resp.json()
+    for commit in data[0:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
