c19e629aba917a42432ac866dba33151deb01a98
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-09-17 10:16:24 +0000

    minor updates

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-// Parse the first argument passed to the script and convert it to an integer
-const arg = parseInt(process.argv[2]);
+// Get the first argument passed to the script
+const x = parseInt(process.argv[2]);
 
-// Check if the argument is a valid number and greater than zero
-if (isNaN(arg) || arg <= 0) {
-  // If the argument is not a number or less than or equal to zero, print an error message
+// Check if x is a valid number
+if (isNaN(x)) {
+  // Print error message if it's not a number
   console.log('Missing number of occurrences');
 } else {
-  // If the argument is a valid positive number, print "C is fun" that many times
-  for (let i = 0; i < arg; i++) {
+  // Loop x times and print 'C is fun'
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
