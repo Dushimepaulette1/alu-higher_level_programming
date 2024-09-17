38fc134cc3fe9a2daaa3804b6184391d97791059
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-09-17 09:18:31 +0000

    javascript

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let first-line = 'C is fun';
+let second-line = 'Python is cool';
+let third-line = 'JavaScript is amazing';
+console.log(first-line);
+console.log(second-line);
+console.log(third-line);
