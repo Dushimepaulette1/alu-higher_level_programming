03d21af6b20fe5a257f68a6d96185302c272917c
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-09-27 04:22:06 -0700

    js add ons

---
+++ javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// Import the required modules
+const fs = require('fs');
+
+// Get the file paths from the command-line arguments
+const [,, fileA, fileB, fileC] = process.argv;
+
+// Read the contents of the first file
+fs.readFile(fileA, 'utf8', (err, dataA) => {
+  if (err) {
+    console.error(`Error reading ${fileA}: ${err.message}`);
+    return;
+  }
+  
+  // Read the contents of the second file
+  fs.readFile(fileB, 'utf8', (err, dataB) => {
+    if (err) {
+      console.error(`Error reading ${fileB}: ${err.message}`);
+      return;
+    }
+
+    // Concatenate the contents and write to the destination file
+    fs.writeFile(fileC, dataA + dataB, (err) => {
+      if (err) {
+        console.error(`Error writing to ${fileC}: ${err.message}`);
