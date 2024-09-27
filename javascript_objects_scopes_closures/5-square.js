8c2d89ebb23b1524e05bde0258fa701c1e37a5b9
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-09-27 04:06:11 -0700

    js

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// Import the Rectangle class from the previous file
+const Rectangle = require('./4-rectangle');
+
+// Define a class Square that inherits from Rectangle
+class Square extends Rectangle {
+  // Constructor for Square, takes 1 argument: size
+  constructor(size) {
+    // Call the constructor of Rectangle with size for both width and height
+    super(size, size);
+  }
+}
+
+// Export the Square class to be used in other files
+module.exports = Square;
+
