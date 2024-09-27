1657b53e4ff138031f843f34ffcddb90d30f78c1
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-09-27 01:49:59 -0700

    js

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+// Define a class called Rectangle
+class Rectangle {
+  // This class is currently empty
+  // You can add properties and methods later if needed
+}
+
+// Export the Rectangle class so it can be used in other files
+module.exports = Rectangle;
