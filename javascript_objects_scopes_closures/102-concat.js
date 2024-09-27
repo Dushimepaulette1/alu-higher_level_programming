e61e63dd03ab115eb8a03da5b2a473567729e9e6
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-09-27 04:38:03 -0700

    js

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 // Get the command-line arguments (the file paths)
 const args = process.argv.slice(2);
 
-// Read the contents of the first file synchronously
+// Read the contents of the first file synchronously, with 'utf8' encoding
 const fileA = fs.readFileSync(args[0], 'utf8');
 
-// Read the contents of the second file synchronously
+// Read the contents of the second file synchronously, with 'utf8' encoding
 const fileB = fs.readFileSync(args[1], 'utf8');
 
 // Concatenate the contents of both files
@@ -17,5 +17,5 @@ const fileC = `${fileA}${fileB}`;
 // Write the concatenated content to the destination file synchronously
 fs.writeFileSync(args[2], fileC);
 
-// Print a message to indicate successful concatenation
-console.log(`Contents of ${args[0]} and ${args[1]} have been concatenated into ${args[2]}`);
+// Commented out the console log to match the expected output
+// console.log(`Contents of ${args[0]} and ${args[1]} have been concatenated into ${args[2]}`);
