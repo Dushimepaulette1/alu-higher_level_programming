55543e3db60d49e7a37f6ce2eb15772501b7f9c0
Dushimepaulette1 <p.dushime1@alustudent.com>
2024-09-27 04:17:25 -0700

    js updates

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -4,7 +4,7 @@ let counter = 0;
 // Export a function named logMe
 exports.logMe = function (item) {
 // Print the current count (number of arguments already printed) and the current argument value
-  console.log(`${counter}: ${item}`); 
-// Increment the counter after printing the current argument
+  console.log(`${counter}: ${item}`);
+  // Increment the counter after printing the current argument
   counter++;
 };
